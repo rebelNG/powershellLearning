a5dfd43a51a38088c217cd7f51bb8c4cd4ba4bfd
Emmanuel Alozieuwa <emmanuelalozieuwa@gmail.com>
2023-12-19 15:28:24 +0000

    Creating Answerfile

---
+++ Test/ServiceRemoval.ps1
@@ -0,0 +1,59 @@
+<#
+    Script created by Emmanuel Alozieuwa
+    on 18th December, 2023
+    Script is designed to help in service/services removal
+#>
+
+<#[CmdletBinding()]
+param (
+    [Parameter(Mandatory=$true)]
+    [string[]]
+    $ServicesName
+)#>
+
+Write-Output ""
+
+$Location = Split-Path $MyInvocation.MyCommand.Path
+
+$DMLLocation = "\\csdatg04\Users\CDC Wintel\Automation_Scripts\Service Removal"
+$FileLocation = $DMLLocation + "\Service Removal script"
+
+$CurrentFileLocation = Get-Content -Path "$Location\CurrentLocation.txt"
+$CurrentFileLocation = $CurrentFileLocation.Trim()
+
+$CurrentUser = [System.Security.Principal.WindowsIdentity]::GetCurrent().name
+$CurrentUser = $CurrentUser.split("\")
+$CurrentUser = $CurrentUser[1]
+
+foreach ($Service in $ServicesName){
+    # Check if the service name is provided
+    if ($Service) {
+        # Check if the website exists
+        $website = Get-Website -Name $Service -ErrorAction SilentlyContinue
+        if ($website) {
+            # Remove the website
+            Remove-Website -Name $Service
+            Write-Host "Website '$Service' has been removed." -ForegroundColor White
+        } else {
+            Write-Host "Website '$Service' does not exist or has already been removed." -ForegroundColor Red
+        }
+
+        # Test if folderpaths exist
+        $folderPath1 = "B:\Releases\$Service"
+        $folderPath2 = "E:\inetpub\wwwroot\$Service"
+        $folderPath3 = "L:\IIS\$Service"
+
+        $folders = @($folderPath1, $folderPath2, $folderPath3)
+
+        foreach ($folder in $folders) {
+            if (Test-Path -Path $folder -ErrorAction SilentlyContinue) {
+                Remove-Item -Path $folder -Force -Recurse
+                Write-Host "Folder '$Service' has been removed from: $folder" -ForegroundColor Green
+            } else {
+                Write-Host "Folder '$Service' does not exist at: $folder" -ForegroundColor Red
+            }
+        }
+    } else {
+        Write-Host "Please provide a valid website name." -ForegroundColor Red
+    }
+}
